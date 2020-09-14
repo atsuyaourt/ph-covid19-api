03bb6ae5abab03511a2e4ed478f70016a31c1462
Emilio Gozo <emil.gozo@gmail.com>
2020-09-14 21:18:12 +0800

    docs: add swagger docs for case

--- src/docs/swaggerDef.js
+++ src/docs/swaggerDef.js
@@ -1,21 +1,21 @@
-const { version } = require('../../package.json');
+const {
+  version
+} = require('../../package.json');
 const config = require('../config/config');
 
 const swaggerDef = {
   openapi: '3.0.0',
   info: {
-    title: 'node-express-mongoose-boilerplate API documentation',
+    title: 'PH COVID-19 API documentation',
     version,
     license: {
       name: 'MIT',
-      url: 'https://github.com/hagopj13/node-express-mongoose-boilerplate/blob/master/LICENSE',
+      url: 'https://github.com/emiliogozo/ph-covid19-api/blob/master/LICENSE',
     },
   },
-  servers: [
-    {
-      url: `http://localhost:${config.port}/v1`,
-    },
-  ],
+  servers: [{
+    url: `http://localhost:${config.port}/v1`,
+  }, ],
 };
 
 module.exports = swaggerDef;
